fe7b6c0f635f1dbd57c17b1350c77e33d88752d2
Andrew Medvedev <misterdecoy@mail.ru>
2015-10-06 12:38:28 +0300

    Initial Commit

---
+++ CoreDataPlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataPlayground
+//
+//  Created by Andrew Medvedev on 06/10/15.
+//  Copyright © 2015 Andrew Medvedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataPlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataPlayground
+//
+//  Created by Andrew Medvedev on 06/10/15.
+//  Copyright © 2015 Andrew Medvedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
